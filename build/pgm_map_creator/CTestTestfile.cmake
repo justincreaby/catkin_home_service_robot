b6b8cb917845967059420ce4a3bf2ceeb0284e52
justin creaby <justincreaby@gmail.com>
2022-08-27 16:57:23 -0600

    working solution

--- build/pgm_map_creator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justincreaby/catkin_where_am_i/src/pgm_map_creator
-# Build directory: /home/justincreaby/catkin_where_am_i/build/pgm_map_creator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(msgs)
--- devel/setup.sh
+++ devel/setup.sh
@@ -11,7 +11,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/justincreaby/catkin_where_am_i/devel}
+: ${_CATKIN_SETUP_DIR:=/home/parallels/catkin_where_am_i/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
